edbd407122127c417d1768c546c144c141fc2ada
Ryota KITA <snptkdn@snptkdn.local>
2023-08-14 02:04:13 +0900

    検索中や検索前のUIを実装した。

--- src/routes/(pages)/pages/search/+page.svelte
+++ src/routes/(pages)/pages/search/+page.svelte
@@ -2,6 +2,7 @@
 	import { each } from 'svelte/internal';
 	import { writable } from 'svelte/store';
 	import { Icon } from 'svelte-ionicons';
+	import { ProgressRadial } from '@skeletonlabs/skeleton';
 	import BookSearchCard from '../../../components/BookSearchCard.svelte';
 	import fetchSearchBooks from '../../../serch_books';
 
@@ -25,6 +26,9 @@
 </div>
 
 {#if $loading}
+	<div class="flex justify-center mt-12">
+		<ProgressRadial class="w-11" />
+	</div>
 {:else if Array.isArray($data.items)}
 	{#each $data.items as book}
 		<BookSearchCard
@@ -37,4 +41,24 @@
 			}}
 		/>
 	{/each}
+{:else}
+	<div class="flex justify-center flex-wrap">
+		<Icon class="mt-10 text-slate-400" name="book-outline" width="80" height="80" />
+		<div class="w-full" />
+		<span class="my-5 text-slate-400">書籍を探しましょう！</span>
+		<div class="w-full" />
+		<div class="border border-dashed p-4 border-slate-700 border-2 rounded-lg">
+			<div class="flex">
+				<Icon class="text-slate-400" name="help-circle-outline" />
+				<span class="ml-1 text-slate-400">書籍が見つかりませんか？</span>
+			</div>
+			<div class="w-full" />
+			<div class="flex mt-3 justify-center">
+				<button type="button" class="chip variant-ghost">
+					<Icon class="text-slate-400" name="add-circle-outline" />
+					<span>手動で登録する</span>
+				</button>
+			</div>
+		</div>
+	</div>
 {/if}
