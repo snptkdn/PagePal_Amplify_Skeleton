f5bc8d720fa3a6f9c6f55a2d7687da058d716c14
Ryota KITA <snptkdn@snptkdn.local>
2023-08-19 00:35:01 +0900

    不要なレイアウトを削除した。

--- src/routes/(root)/+layout.svelte
+++ src/routes/(root)/+layout.svelte
@@ -24,32 +24,6 @@
 					<strong class="text-xl uppercase"> PagePal </strong>
 				</slot>
 			</svelte:fragment>
-			<svelte:fragment slot="trail">
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Discord
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
-					target="_blank"
-					rel="noreferrer"
-				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
-				</a>
-			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
 	<!-- Page Route Content -->
